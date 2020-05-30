ab61ff89e1ea923b287e32256016361e5f8540c0
MohamedAzar <azartechnet@gmail.com>
2020-05-30 16:12:15 +0530

    30-05-2020

---
+++ Spring-Example-Revision/Spring-Example-Revision/src/main/java/com/xyz/SpringExampleRevision/SpringExampleRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.SpringExampleRevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleRevisionApplication.class, args);
+	}
+
+}
---
+++ Spring-Example-Revision/Spring-Example-Revision/src/test/java/com/xyz/SpringExampleRevision/SpringExampleRevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.SpringExampleRevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleRevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
