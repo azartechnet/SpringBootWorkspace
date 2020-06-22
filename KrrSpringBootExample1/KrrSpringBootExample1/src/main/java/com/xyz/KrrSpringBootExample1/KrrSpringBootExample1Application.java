81849e0d29b250ba879eb240e932ec7683ac94b9
MohamedAzarudeen <azartechnet@gmail.com>
2020-06-22 16:23:19 +0530

    22-06-2020

---
+++ KrrSpringBootExample1/KrrSpringBootExample1/src/main/java/com/xyz/KrrSpringBootExample1/KrrSpringBootExample1Application.java
@@ -0,0 +1,13 @@
+package com.xyz.KrrSpringBootExample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrrSpringBootExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrrSpringBootExample1Application.class, args);
+	}
+
+}
---
+++ KrrSpringBootExample1/KrrSpringBootExample1/src/test/java/com/xyz/KrrSpringBootExample1/KrrSpringBootExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.KrrSpringBootExample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrrSpringBootExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
